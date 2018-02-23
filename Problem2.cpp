da4b325e0bede07ac05b2d0fceb8fdfe0bea671f
alfredlima <alfredolms@ic.ufal.br>
2018-02-23 03:49:17 -0300

    Create table and function

--- Problem2.cpp
+++ Problem2.cpp
@@ -7,6 +7,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+map< string , map< string , double> > table;
+vector< string > lines;
+
 struct node {
     string name;
     map< string , vector<string> > edges;
@@ -23,6 +26,7 @@ struct node {
 
 map< string , node > nodes;
 
+// Criação dos nós
 void createNodes(){
     for (int i = 1; i < 15 ; ++i) {
         string name = "E" + to_string(i);
@@ -30,6 +34,7 @@ void createNodes(){
     }
 }
 
+// Criação das conexões
 void createConnections(){
     // Nós da linha azul
     int blue[] = {1,2,3,4,5,6};
@@ -81,6 +86,45 @@ void createConnections(){
 
 }
 
-int main(){
+// Leitura da tabela
+void readFile(){
+    FILE *fp;
+    fp = fopen("Problem2.in", "r+");
+    int c;
+    for (int i = 0; i < 14; ++i) {
+        for (int j = 0; j < 14; ++j) {
+            fscanf(fp , "%d", &c );
+            table[ "E" + to_string(i+1) ][ "E" + to_string(j+1) ] = c;
+        }
+    }
+}
 
+// Criação das linhas
+void createLines(){
+    lines.push_back( "blue" );
+    lines.push_back( "red" );
+    lines.push_back( "yellow" );
+    lines.push_back( "green" );
+}
+
+void init(){
+    readFile();
+    createNodes();
+    createConnections();
+}
+
+double avaliacao( string from , string target ){
+    return table[from][target]/30.0;
+}
+
+void aStar( string from , string target ){
+    map< string , double > dist;
+    priority_queue< tuple<double,double,string,string> > pq;
+    for ( auto l : lines ){
+        pq.push( make_tuple( avaliacao(from,target) , 0 , l , from ) );
+    }
+}
+
+int main(){
+    init();
 }
